bd21bf272d0066a47533da0b8d0a8ef9f327a85a
Eli <158611286+CapybarasUnited@users.noreply.github.com>
2024-11-10 16:15:51 -0800

    1

---
+++ app/src/main/java/com/cs460/finalprojectfirstdraft/List.java
@@ -0,0 +1,10 @@
+package com.cs460.finalprojectfirstdraft;
+
+public class List {
+    private List list;
+    private int listID;
+
+    public List(int listID) {
+        this.listID = listID;
+    }
+}
