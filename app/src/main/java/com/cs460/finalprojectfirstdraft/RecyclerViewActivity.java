702f90886244ce713256be088c4d6288955d1f69
Ali Nizar <alinizar97@gmail.com>
2024-11-17 19:58:56 -0800

    Added javadocs for ListItem.java, RecyclerViewActivity.java, RecyclerViewAdapter.java.

--- app/src/main/java/com/cs460/finalprojectfirstdraft/RecyclerViewActivity.java
+++ app/src/main/java/com/cs460/finalprojectfirstdraft/RecyclerViewActivity.java
@@ -1,5 +1,4 @@
 package com.cs460.finalprojectfirstdraft;
-
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +8,21 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Activity class for displaying a RecyclerView with a list of items.
+ */
 public class RecyclerViewActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerViewAdapter adapter;
     private List<ListItem> itemList;
 
+    /**
+     * Called when the activity is starting.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after previously being shut down,
+     *                           this Bundle contains the data it most recently supplied.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,17 +30,16 @@ public class RecyclerViewActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerView);
 
-        // Initialize list and add some sample data
+        // Initialize the list and add some sample data
         itemList = new ArrayList<>();
         itemList.add(new ListItem("Item 1"));
         itemList.add(new ListItem("Item 2"));
         itemList.add(new ListItem("Item 3"));
         itemList.add(new ListItem("Item 4"));
 
-        // Set up the RecyclerView
+        // Set up the RecyclerView with the adapter
         adapter = new RecyclerViewAdapter(itemList);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
     }
 }
-
--- app/src/main/java/com/cs460/finalprojectfirstdraft/RecyclerViewAdapter.java
+++ app/src/main/java/com/cs460/finalprojectfirstdraft/RecyclerViewAdapter.java
@@ -10,14 +10,29 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+/**
+ * Adapter class for managing the data and binding it to the RecyclerView.
+ */
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
     private List<ListItem> itemList;
 
+    /**
+     * Constructor for RecyclerViewAdapter.
+     *
+     * @param itemList The list of items to be displayed in the RecyclerView.
+     */
     public RecyclerViewAdapter(List<ListItem> itemList) {
         this.itemList = itemList;
     }
 
+    /**
+     * Called when a new ViewHolder is created.
+     *
+     * @param parent   The parent ViewGroup.
+     * @param viewType The type of the new view.
+     * @return A new ViewHolder that holds a View for each list item.
+     */
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -26,20 +41,39 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return new ViewHolder(view);
     }
 
+    /**
+     * Called to display data at a specific position.
+     *
+     * @param holder   The ViewHolder which should be updated to display the contents.
+     * @param position The position of the item within the data set.
+     */
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         ListItem item = itemList.get(position);
         holder.titleTextView.setText(item.getTitle());
     }
 
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The number of items.
+     */
     @Override
     public int getItemCount() {
         return itemList.size();
     }
 
+    /**
+     * ViewHolder class to hold the views for each list item.
+     */
     public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView titleTextView;
 
+        /**
+         * Constructor for ViewHolder.
+         *
+         * @param itemView The View of the list item.
+         */
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             titleTextView = itemView.findViewById(R.id.itemTitle);
